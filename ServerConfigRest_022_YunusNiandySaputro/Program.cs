4c434fa3f804f31b0c2600c339ed562f879a1d4b
ReAlpha39 <yunusw718@gmail.com>
2020-12-19 22:25:36 +0800

    Add project files.

---
+++ ServerConfigRest_022_YunusNiandySaputro/Program.cs
@@ -0,0 +1,34 @@
+﻿using ServiceRest_022_YunusNiandySaputro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_022_YunusNiandySaputro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObject = null;
+
+            try
+            {
+                hostObject = new ServiceHost(typeof(TI_UMY));
+                hostObject.Open();
+                Console.WriteLine(hostObject);
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
